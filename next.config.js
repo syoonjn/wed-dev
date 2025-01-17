6d68bbb80453810269d91887f11ea3cbe82f2590
soyj95 <https://soyj95@bitbucket.org>
2025-01-17 21:12:22 +0900

    테스트

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-// next.config.js
-
 const isProd = process.env.NODE_ENV === 'production';
-
-const repoName = 'wed-dev'; // GitHub 리포지토리 이름으로 변경하세요
+const repoName = 'wed-dev'; // GitHub 리포지토리 이름
 
 module.exports = {
     output: 'export',
     basePath: isProd ? `/${repoName}` : '',
-    assetPrefix: isProd ? `/${repoName}` : '',
+    assetPrefix: isProd ? `/${repoName}/` : '',
     images: {
         unoptimized: true, // 이미지 최적화 비활성화
     },
