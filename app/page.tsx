4a893950c216d64ed247c3b521078f5c0aed7494
soyj95 <https://soyj95@bitbucket.org>
2025-01-17 20:23:36 +0900

    production 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+const isProd = process.env.NODE_ENV === 'production';
+console.log('Running in production:', isProd, process.env.NODE_ENV);
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
