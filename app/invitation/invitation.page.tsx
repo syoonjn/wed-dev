4c190acfaa9bf8831303b262bc6339d6c8ac9b8a
soyj95 <https://soyj95@bitbucket.org>
2025-01-18 13:22:58 +0900

    스타일 변경

--- app/invitation/invitation.page.tsx
+++ app/invitation/invitation.page.tsx
@@ -1,23 +1,21 @@
+"use client"
+
+import { Dropdown } from "flowbite-react";
 import React from "react";
 
 // Next.js PageProps 확장 가능하도록 설정
-type PageProps = {
-    [key: string]: unknown; // 확장 가능하도록 제한 완화
-};
+// type PageProps = {
+//     [key: string]: unknown; // 확장 가능하도록 제한 완화
+// };
 
 // InvitationProps 정의
 type InvitationProps = {
     name?: string;
     date?: string;
     location?: string;
-} & PageProps; // PageProps와 병합
-
+}; // PageProps와 병합
 // InvitationPage 컴포넌트
-const InvitationPage: React.FC<InvitationProps> = ({
-                                                       name = "Guest",
-                                                       date = "January 17, 2025",
-                                                       location = "Seoul, Korea",
-                                                   }) => {
+const InvitationPage = ({ name = 'test', date = 'date', location = 'location' }: InvitationProps) => {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen bg-pink-50">
             <div className="text-center p-8 bg-white rounded-lg shadow-lg max-w-md">
@@ -31,6 +29,12 @@ const InvitationPage: React.FC<InvitationProps> = ({
                     <p className="text-lg text-gray-500">Date: {date}</p>
                     <p className="text-lg text-gray-500">Location: {location}</p>
                 </div>
+                <Dropdown label="Dropdown button">
+                    <Dropdown.Item>Dashboard</Dropdown.Item>
+                    <Dropdown.Item>Settings</Dropdown.Item>
+                    <Dropdown.Item>Earnings</Dropdown.Item>
+                    <Dropdown.Item>Sign out</Dropdown.Item>
+                </Dropdown>
             </div>
         </main>
     );
--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'wed-dev'; // GitHub 리포지토리 이름
+const repoName = 'wed-dev';
 
 module.exports = {
     output: 'export',
     basePath: isProd ? `/${repoName}` : '',
     assetPrefix: isProd ? `/${repoName}/` : '',
     images: {
-        unoptimized: true, // 이미지 최적화 비활성화
+        unoptimized: true,
     },
 };
