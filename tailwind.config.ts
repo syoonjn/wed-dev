b4b333b4b8db4c226c610265172cc31882502f7d
soyj95 <https://soyj95@bitbucket.org>
2025-01-18 12:24:23 +0900

    flowbite > nextjs config 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,18 @@
 import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
 
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // App Router 경로
     "./pages/**/*.{js,ts,jsx,tsx}", // Pages Router 경로
     "./components/**/*.{js,ts,jsx,tsx}", // 컴포넌트 경로
-    "./node_modules/flowbite-react/**/*.js", // Flowbite-React 경로 추가
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("flowbite/plugin"), // Flowbite 플러그인 추가
+    flowbite.plugin(),
   ],
 };
 
