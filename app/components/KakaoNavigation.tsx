88f9f5da8ec784abf0890843b236dc0b407f1864
syoonj <syoonjn@gmail.com>
2025-04-12 00:23:32 +0900

    네이버 지도 테스트

--- app/components/KakaoNavigation.tsx
+++ app/components/KakaoNavigation.tsx
@@ -33,6 +33,30 @@ const KakaoNavigation = () => {
         });
     };
 
+    const handleClick = () => {
+        const lat = 36.7935047; // 목적지 위도
+        const lng = 127.1047214; // 목적지 경도
+        const name = encodeURIComponent("CA웨딩컨벤션");
+        // const appUrl = `nmap://route/public?dlat=${lat}&dlng=${lng}&dname=${name}`;
+        const appUrl = `nmap://route/walk?dlat=${lat}&dlng=${lng}&dname=${name}&appname=myweb.app`;
+
+        const webUrl = `https://map.naver.com/v5/directions/-/-/${lng},${lat},${name},PLACE_POI/-?c=15.00,0,0,0,dh`;
+
+        // 모바일에서 네이버 지도 앱으로 연결, 실패 시 웹으로 fallback
+        window.location.href = appUrl;
+
+        // 앱 미설치 대비 fallback 타이머
+        setTimeout(() => {
+            window.location.href = webUrl;
+        }, 1500);
+    };
+
+    const handleClick2 = () => {
+        const naverApp = 'nmap://route/car?slat=37.4640070&slng=126.9522394&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=37.5209436&dlng=127.1230074&dname=%EC%98%AC%EB%A6%BC%ED%94%BD%EA%B3%B5%EC%9B%90&appname=https://syoonjn.github.io/wed-dev/';
+        window.location.href = naverApp;
+    }
+
+
     return (
         <div className="flex items-center justify-center border-t p-4">
             {/* 네이버 지도 */}
@@ -54,7 +78,7 @@ const KakaoNavigation = () => {
 
             {/* 카카오 내비 */}
             <div
-                onClick={startNavigation}
+                onClick={handleClick}
                 className="flex flex-1 cursor-pointer items-center justify-center hover:opacity-80"
             >
                 <Image
@@ -71,7 +95,7 @@ const KakaoNavigation = () => {
 
             {/* 티맵 */}
             <div
-                onClick={startNavigation}
+                onClick={handleClick2}
                 className="flex flex-1 cursor-pointer items-center justify-center hover:opacity-80"
             >
                 <Image
