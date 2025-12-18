dd3ae7daa5e0dfe7548eefc7d4f5d7cd3d9823fa
soyj95 <https://soyj95@bitbucket.org>
2025-12-18 21:36:27 +0900

    로그 삭제

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
-console.log('SUPABASE_URL =>', SUPABASE_URL);
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // 데이터 삽입 함수
