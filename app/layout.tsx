cd144c4fa0b1e721db3adc3386f5e536d2b01364
soyj95 <https://soyj95@bitbucket.org>
2025-12-19 19:33:55 +0900

    fix: - 줌 기능 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <head>
         <ThemeModeScript />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(eventStructuredData) }}
